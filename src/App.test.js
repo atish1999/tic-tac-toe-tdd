859ae68a4308bf08f05c38e00139a00c323882f0
Atish Kumar Naskar <atishnaskar1999@gmail.com>
2023-07-09 19:59:19 +0530

    Add. tic-tac-toe rendering test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import Board from './components/Board';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+describe("Rendering test of tic-tac-toe-board", () => {
+
+
+  it("Should render a tic-tac-toe-board containing 9 squares", () => {
+    render(<Board />);
+    const squareComponent = screen.getAllByRole("button");
+    expect(squareComponent).toHaveLength(9);
+
+  })
+
+  it("Should render a tic-tac-toe-board containing 3 rows", () => {
+    render(<Board />);
+
+    const boardRowComponent = screen.getAllByTestId("board-row");
+
+    expect(boardRowComponent).toHaveLength(3);
+  })
+
+  it("Should render next player is X on status", () => {
+    render(<Board />);
+
+    const status = screen.getByText("Next Turn: X");
+
+    expect(status).toBeInTheDocument();
+
+  })
+
+  it("Should render next player is O on status", () => {
+    render(<Board />);
+    fireEvent.click(screen.getByTestId("square-0"));
+
+    const status = screen.getByText("Next Turn: O");
+
+    expect(status).toBeInTheDocument();
+
+  })
+
+
 });
