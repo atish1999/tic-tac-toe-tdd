263abd4454545e0fc5d4ef0debf280995ca56e79
Atish Kumar Naskar <atishnaskar1999@gmail.com>
2023-07-09 20:36:31 +0530

    Add. test sqaure component shuold render X/O on click in tic-tac-toe testing suite

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,6 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 import Board from './components/Board';
 
-
-
 describe("Rendering test of tic-tac-toe-board", () => {
 
 
@@ -12,7 +10,7 @@ describe("Rendering test of tic-tac-toe-board", () => {
     const squareComponent = screen.getAllByRole("button");
     expect(squareComponent).toHaveLength(9);
 
-  })
+  });
 
   it("Should render a tic-tac-toe-board containing 3 rows", () => {
     render(<Board />);
@@ -20,7 +18,7 @@ describe("Rendering test of tic-tac-toe-board", () => {
     const boardRowComponent = screen.getAllByTestId("board-row");
 
     expect(boardRowComponent).toHaveLength(3);
-  })
+  });
 
   it("Should render next player is X on status", () => {
     render(<Board />);
@@ -29,7 +27,7 @@ describe("Rendering test of tic-tac-toe-board", () => {
 
     expect(status).toBeInTheDocument();
 
-  })
+  });
 
   it("Should render next player is O on status", () => {
     render(<Board />);
@@ -39,7 +37,40 @@ describe("Rendering test of tic-tac-toe-board", () => {
 
     expect(status).toBeInTheDocument();
 
+  });
+
+
+});
+
+
+describe("tic-tac-toe-board functionality testing", () => {
+
+  it("should render X on odd number of clicks", () => {
+
+    render(<Board />);
+
+    const index = parseInt(Math.random() * 8);
+    const firstSquareIndex = "square-" + index;
+    fireEvent.click(screen.getByTestId(firstSquareIndex));
+
+    expect(screen.getByTestId(firstSquareIndex)).toHaveTextContent("X");
+
   })
 
+  it("should render O on even number of clicks", () => {
+
+    render(<Board />);
+
+    const oddIndex = parseInt((Math.random() * 2) + 1);
+    const evenIndex = oddIndex + 1;
+    const oddClickIndex = "square-" + oddIndex;
+    const evenClickIndex = "square-" + evenIndex;
+    
+    fireEvent.click(screen.getByTestId(oddClickIndex));
+    fireEvent.click(screen.getByTestId(evenClickIndex));
+
+    expect(screen.getByTestId(evenClickIndex)).toHaveTextContent("O");
+
+  })
 
 });
